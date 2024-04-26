ce089dd42f42e82f0c135accca96027924ea5af7
Totemy <thetotemofficial@gmail.com>
2024-04-26 03:11:25 +0300

    Delete useless files

--- src/components/Home/CatalogComponent.vue
+++ src/components/Home/CatalogComponent.vue
@@ -2,11 +2,11 @@
 <section>
   <h2>Products</h2>
   <ul>
-    <li v-for="product in products" :key="product.id">{{ product.name }} </li>
+    <li v-for="product in products" :key="product.id">{{ product.name }} 2 </li>
   </ul>
   <h2>Categories</h2>
   <ul>
-    <li v-for="category in categories" :key="category.id">{{ category.name }}</li>
+    <li v-for="category in categories" :key="category.id">{{ category.name }} 3</li>
   </ul>
 </section>
 </template>
