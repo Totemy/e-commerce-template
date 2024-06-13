29b9f0573083ab8ea603de92221a48403c326e2c
Totemy <thetotemofficial@gmail.com>
2024-06-14 02:35:04 +0300

    Edited Home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,30 @@
 <template>
   <main>
     <div>
-      <h1>{{ title }}</h1>
+      <div>
+        Carousel with images
+      </div>
+      <div>
 
+      </div>
+      <div>
+        <CategoryView/>
+      </div>
+      <div>
+
+      </div>
     </div>
   </main>
 </template>
 <script>
 
+import CategoryView from '@/components/Home/CategoryView.vue'
+
 export default {
+  components: { CategoryView },
   data(){
     return{
-      title: 'Shop cart'
+
     }
   }
 }
