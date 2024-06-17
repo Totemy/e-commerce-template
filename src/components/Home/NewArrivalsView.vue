a5fd03d19754fdbc14e36ac77d25925a1d3825fe
Totemy <thetotemofficial@gmail.com>
2024-06-17 23:25:26 +0300

    added arriavals

---
+++ src/components/Home/NewArrivalsView.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <div class="new-arrivals">
+      <div class="new-arrivals__header">
+        <h2>
+          New Arrivals
+        </h2>
+        <div>
+          More Products ->
+        </div>
+      </div>
+      <div class="new-arrivals__content">
+        <div>
+
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
