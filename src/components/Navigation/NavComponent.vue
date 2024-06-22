74ea918b9abc8053f4d311ae08faadd680aaa807
Totemy <thetotemofficial@gmail.com>
2024-06-22 05:31:26 +0300

    Added navbar

--- src/components/Navigation/NavComponent.vue
+++ src/components/Navigation/NavComponent.vue
@@ -1,13 +1,37 @@
 <template>
   <section class="container">
     <nav class="navbar">
-      <router-link class="navbar__link" to="/">Home</router-link>
-      <router-link class="navbar__link" to="/catalog">Shop</router-link>
-      <router-link class="navbar__link" to="/catalog">Product</router-link>
-      <router-link class="navbar__link" to="/catalog">Contact Us</router-link>
-      <router-link class="navbar__link" to="/admin">Admin</router-link>
+      <div class="navbar__logo">
+        3legant<span class="color-n4">.</span>
+      </div>
+      <div class="navbar__links">
+        <router-link class="navbar__link" to="/" :class="{ 'navbar__link-active': activeRoute === '/' }" >Home</router-link>
+        <router-link class="navbar__link" to="/catalog" :class="{ 'navbar__link-active': activeRoute === '/catalog' }">Shop</router-link>
+        <router-link class="navbar__link" to="/catalog" :class="{ 'navbar__link-active': activeRoute === '/catalog' }" >Product</router-link>
+        <router-link class="navbar__link" to="/catalog" :class="{ 'navbar__link-active': activeRoute === '/catalog' }">Contact Us</router-link>
+        <router-link class="navbar__link" to="/admin" :class="{ 'navbar__link-active': activeRoute === '/admin' }" >Admin</router-link>
+      </div>
+     <div class="navbar__buttons">
+          <button class="btn btn__nav"> <img src="/icons/search.svg" alt="search" /> </button>
+          <button class="btn btn__nav"><img src="/icons/account.svg" alt="account" /> </button>
+          <button class="btn btn__nav"> <img src="/icons/bag.svg" alt="shop-bag" /> </button>
+     </div>
     </nav>
   </section>
 </template>
-<script setup>
+<script>
+export default {
+  data() {
+    return {
+      activeRoute: '', // Initialize active route
+      openBar: false
+    };
+  },
+  watch: {
+    '$route'(to) {
+      this.activeRoute = to.path;
+      this.openBar = false;
+    }
+  }
+}
 </script>
