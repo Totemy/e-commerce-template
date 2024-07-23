8fbac824f3432ac5c55c5b7107e48b67cc236fd9
Totemy <thetotemofficial@gmail.com>
2024-07-23 23:19:55 +0300

    Connection price filter to Filter

--- src/components/Shop/FilterCategoryComponent.vue
+++ src/components/Shop/FilterCategoryComponent.vue
@@ -21,13 +21,31 @@
 </template>
 <script setup>
 import {useCategoriesStore} from '@/store/modules/categories.js'
-import { computed, onMounted } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import PriceFilterComponent from '@/components/Shop/PriceFilterComponent.vue'
+
+const activeRange = ref({label: 'All', min: null, max: null});
+const filterProducts = computed(() => {
+  if(!activeRange.min && activeRange.max){
+    return products;
+  }
+  return products.filter(product => {
+    return product.price >= (activeRange.min ?? 0) && product.price <= (activeRange.max ?? Infinity)
+  })
+})
+const filterProduct = (range) =>{
+  activeRange.min = range.min;
+  activeRange.max = range.max;
+}
+const priceFilterComponent = ref(null);
 
 const store = useCategoriesStore();
 
 onMounted(()=>{
   store.fetchCategories();
+  if(priceFilterComponent.value){
+    priceFilterComponent.value.$on('range-selected', filterProducts)
+  }
 })
 const categories = computed(()=> store.allCategories)
 
