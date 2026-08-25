441bd9819bb24bd010085c33e5eda91b66136dfd
Totemy <thetotemofficial@gmail.com>
2026-08-25 17:25:58 +0300

    Add scroll reveal directive

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import { firebaseApp } from '@/firebase.js'
 import { createPinia } from 'pinia';
+import { reveal } from '@/directives/reveal.js'
 
 const app = createApp(App)
 const pinia = createPinia();
@@ -11,4 +12,5 @@ firebaseApp;
 
 app.use(router)
 app.use(pinia)
+app.directive('reveal', reveal)
 app.mount('#app')
